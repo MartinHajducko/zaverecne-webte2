f040fa485611b4c2ab1b18487d1401204cdc03ec
hajdy <xhajducko@stuba.sk>
2023-05-17 20:34:14 +0200

    latex fix

--- latex.php
+++ latex.php
@@ -1,60 +1,3 @@
-
-<?php
-/*
-try {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
-    require_once('config.php');
-
-    $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Read and process the LaTeX file
-    $latexContent = file_get_contents('odozva02pr.tex');
-
-    // Process tasks and solutions
-    $taskPattern = '/\\\\begin\{task\}(.*?)\\\\end\{task\}/s';
-    $solutionPattern = '/\\\\begin\{solution\}(.*?)\\\\end\{solution\}/s';
-
-    preg_match_all($taskPattern, $latexContent, $taskMatches, PREG_SET_ORDER);
-    preg_match_all($solutionPattern, $latexContent, $solutionMatches, PREG_SET_ORDER);
-
-    if (!empty($taskMatches) && !empty($solutionMatches) && count($taskMatches) === count($solutionMatches)) {
-        for ($i = 0; $i < count($taskMatches); $i++) {
-            $taskContent = $taskMatches[$i][1];
-            $solutionContent = $solutionMatches[$i][1];
-
-            // Extract the equation within the task content
-            $equationPattern = '/\\\\begin\{equation\*\}(.*?)\\\\end\{equation\*\}/s';
-            preg_match($equationPattern, $taskContent, $equationMatches);
-
-            // Extract the equation within the solution content
-            $solutionEquationPattern = '/\\\\begin\{equation\*\}(.*?)\\\\end\{equation\*\}/s';
-            preg_match($solutionEquationPattern, $solutionContent, $solutionEquationMatches);
-
-            if (isset($equationMatches[1]) && isset($solutionEquationMatches[1])) {
-                $taskEquation = $equationMatches[1];
-                $solutionEquation = $solutionEquationMatches[1];
-                // Process the equations as needed
-
-                // Insert the task and solution into the database
-                $sql = "INSERT INTO equation (task, solution) VALUES (:task, :solution)";
-                $stmt = $pdo->prepare($sql);
-                $stmt->bindParam(':task', $taskEquation);
-                $stmt->bindParam(':solution', $solutionEquation);
-                $stmt->execute();
-            }
-        }
-    } else {
-        echo "No task content found in the LaTeX file or unequal number of tasks and solutions.";
-    }
-} catch (PDOException $e) {
-    echo "Database Error: " . $e->getMessage();
-}
-*/
-?>
 <?php
 
 try {
@@ -152,8 +95,4 @@ try {
 }} catch (PDOException $e) {
     echo "Database Error: " . $e->getMessage();
 }
-
-
-
-
 ?>
